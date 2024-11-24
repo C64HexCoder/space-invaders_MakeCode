7a20f70f4d412c3b53ddd66f2e449721a8b98b6a
Yossi Shelly <yossi.shelly@gmail.com>
2024-11-24 17:42:45 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,134 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (FireSoot == false) {
+        FireSoot = true
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            `, Canon, 0, -50)
+        music.play(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    }
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    for (let value of Alians) {
+        if (projectile.overlapsWith(value)) {
+            value.startEffect(effects.fire)
+            sprites.destroy(value)
+            sprites.destroy(projectile)
+            FireSoot = false
+            info.setScore(info.score() + 100)
+            music.play(music.melodyPlayable(music.bigCrash), music.PlaybackMode.UntilDone)
+        }
+    }
+})
+let projectile: Sprite = null
+let Canon: Sprite = null
+let Alians: Sprite[] = []
+let FireSoot = false
+let Step = 0.5
+FireSoot = false
+let Direction = 0.01
+scene.setBackgroundImage(assets.image`Background 1`)
+Alians = [sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . 2 2 2 2 2 2 . . . . . 
+    . . . . 2 5 5 5 5 5 5 2 . . . . 
+    . . . 2 5 5 5 5 5 5 5 5 2 . . . 
+    . . . 2 5 5 8 5 5 8 5 5 2 . . . 
+    . . . 2 5 5 5 5 5 5 5 5 2 . . . 
+    . . . 2 5 5 5 5 5 5 5 5 2 . . . 
+    . . . . 2 5 5 5 5 5 5 2 . . . . 
+    . . . . . 2 2 2 2 2 2 . . . . . 
+    . . . . 2 2 . 2 2 . 2 2 . . . . 
+    . . . 2 2 . . 2 2 . . 2 2 . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Enemy), sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 8 8 8 8 . . . . . . 
+    . . . . . 8 8 8 8 8 8 . . . . . 
+    . . . . 8 8 8 8 8 8 8 8 . . . . 
+    . . . . 8 8 8 8 8 8 8 8 . . . . 
+    . . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+    . . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+    . . . 8 8 8 8 8 8 8 8 8 8 . . . 
+    . . . 8 . . . 8 8 . . . 8 . . . 
+    . . . 8 . . . 8 8 . . . 8 . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Enemy)]
+let i = 1
+for (let value of Alians) {
+    value.setPosition(i * 30, 14)
+    i += 1
+}
+Canon = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 8 . . . . . . . . 
+    . . . . . . . 8 . . . . . . . . 
+    . . . . . . 8 8 8 . . . . . . . 
+    . . . . . . 8 8 8 . . . . . . . 
+    . . 8 8 8 8 8 8 8 8 8 8 8 . . . 
+    . 8 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    `, SpriteKind.Player)
+Canon.setPosition(76, 102)
+info.setScore(0)
+music.play(music.stringPlayable("A F E F D G E F ", 100), music.PlaybackMode.UntilDone)
+game.onUpdate(function () {
+    for (let value of Alians) {
+        value.x += Step
+        if (value.x >= 150) {
+            Step = Step * -1
+        }
+        if (value.x <= 10) {
+            Step = Step * -1
+        }
+    }
+    if (projectile) {
+        if (projectile.y <= 0) {
+            FireSoot = false
+        }
+    }
+})
+game.onUpdate(function () {
+    if (controller.left.isPressed()) {
+        if (Canon.x > 10) {
+            Canon.x += -1
+        }
+    } else {
+        if (controller.right.isPressed()) {
+            if (Canon.x < 150) {
+                Canon.x += 1
+            }
+        }
+    }
+})
